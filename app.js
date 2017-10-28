602922599c450353d92d68097f673c812319ef2e
Alejandro Lazo <alazo8807@gmail.com>
2017-10-28 20:49:36 +0000

    Add mongodb and basic routes

--- app.js
+++ app.js
@@ -1,44 +1,103 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp-camp", {useMongoClient: true});
 app.set('view engine', 'ejs');
 app.use( bodyParser.urlencoded({extended: true}));
 
-var campgrounds = [
-   {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-   {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},
-   {name: "Mountain's goeat Rest", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
-   {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-   {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},
-   {name: "Mountain's goeat Rest", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
-   {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-   {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},
-   {name: "Mountain's goeat Rest", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"}
-];
+var campgroundsSchema = new mongoose.Schema({
+    name: String, 
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundsSchema);
+
+// Campground.create({
+//     name: "Salmon Creek",
+//     image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
+//     description: "This is a testing description"
+// }, function(err, camp){
+//     if(err){
+//         console.log("Error adding campground");
+//         console.log(err);
+//     }else{
+//         console.log("campground Added");
+//         console.log(camp);
+//     }
+// });
+
+// var campgrounds = [
+//   {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//   {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},
+//   {name: "Mountain's goeat Rest", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
+//   {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//   {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},
+//   {name: "Mountain's goeat Rest", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
+//   {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//   {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},
+//   {name: "Mountain's goeat Rest", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"}
+// ];
+
    
 app.get('/', function(req,res){
    res.render('landing');
 });
 
 app.get('/campgrounds', function(req,res){
-    res.render('campgrounds', {campgrounds: campgrounds});
+    
+    Campground.find({},function(err, allCamps){
+       if(err){
+           console.log("Could not load campgrounds from db");
+           console.log(err);
+           
+       } else{
+           res.render("index", {campgrounds: allCamps});
+           
+       }
+    });
+
+
 });
 
 app.post('/campgrounds', function(req, res){
     var name = req.body.name;
     var image = req.body.image;
+    var description = req.body.description;
     
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
+    var newCampground = {name: name, image: image, description: description};
     
-    res.render('campgrounds',{campgrounds: campgrounds});
+    //campgrounds.push(newCampground);
+    Campground.create(newCampground, function(err, camp){
+        if(err){
+            console.log("Error adding campground");
+            console.log(err);
+        }else{
+            console.log("Campground added");
+            console.log(camp);
+            res.redirect('/campgrounds');
+        }
+    });
+    
+    
 });
 
 app.get("/campgrounds/new", function(req, res) {
     res.render("new")
 })
 
+app.get("/campgrounds/:id", function(req,res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+       if(err){
+           console.log(err);
+       } else{
+           console.log(foundCampground);
+           res.render("show", {campground: foundCampground});
+       }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('YelpCamp server running');
